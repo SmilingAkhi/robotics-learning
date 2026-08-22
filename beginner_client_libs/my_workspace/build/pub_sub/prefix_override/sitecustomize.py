3c52854bb75dca798c3aa37c8199eb779fa1c988
Abdulrahman Bhadmus <bhabdulrahaman@gmail.com>
2026-08-22 14:22:49 +0100

    A simple ROS2 python publisher and suscriber node

---
+++ beginner_client_libs/my_workspace/build/pub_sub/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/koji/robotics/robotics-learning/beginner_client_libs/my_workspace/install/pub_sub'
