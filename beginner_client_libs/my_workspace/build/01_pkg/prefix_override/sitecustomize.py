14cf0c30afeac00e741e1e055294f631547f4646
Abdulrahman Bhadmus <bhabdulrahaman@gmail.com>
2026-08-22 13:25:48 +0100

    Renamed my ROS2 package using valid naming

--- beginner_client_libs/my_workspace/build/01_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/koji/robotics/robotics-learning/beginner_client_libs/my_workspace/install/01_pkg'
--- beginner_client_libs/my_workspace/build/01_pkg/share/01_pkg/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/koji/robotics/robotics-learning/beginner_client_libs/my_workspace/build/01_pkg"
